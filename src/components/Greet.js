f33fc568a42558faac3371c818931133180114d0
shivanim21 <shivanimaddala212@gmail.com>
2023-11-20 23:56:41 +0530

    src files added

---
+++ src/components/Greet.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// function Greet() {
+//   return (
+//     <div>
+//       Hello Shivani!
+//     </div>
+//   )
+
+// }
+// ES6 Arrow function
+const Greet = () => <h1>Hello!</h1>
+
+export default Greet
